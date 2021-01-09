5150ecbc0bded420972ced665b8d553a4469f5f9
Viacheslav Maksymchuk <mail4maxsl@gmail.com>
2021-01-09 13:55:56 +0200

    first

---
+++ src/components/App.tsx
@@ -0,0 +1,83 @@
+import * as React from 'react';
+
+export class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {
+      currentTask: '',
+      tasks: [],
+    };
+  }
+
+  deleteTask(taskId: number): void {
+    const filteredTasks: Array<ITask> = this.state.tasks.filter(
+      (task) => task.id !== taskId
+    );
+    this.setState({
+      tasks: filteredTasks,
+    });
+  }
+
+  public handleSubmit(e: React.FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    this.setState({
+      currentTask: '',
+      tasks: [
+        ...this.state.tasks,
+
+        {
+          id: this._timeInMilliseconds(),
+          value: this.state.currentTask,
+          completed: false,
+        },
+      ],
+    });
+  }
+
+  // its public becouse return html
+  public renderTask(): JSX.Element[] {
+    return this.state.tasks.map((task: ITask, index: number) => {
+      return (
+        <div key={task.id.toString()}>
+          <span>{task.value}</span>
+          <button onClick={() => this.deleteTask(task.id)}>Delete</button>
+        </div>
+      );
+    });
+  }
+
+  public render(): JSX.Element {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        <h1>React Typescript Todo List</h1>
+        <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input
+            type="text"
+            value={this.state.currentTask}
+            placeholder="Add a task"
+            onChange={(e) => this.setState({ currentTask: e.target.value })}
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <section>{this.renderTask()}</section>
+      </React.Fragment>
+    );
+  }
+
+  private _timeInMilliseconds(): number {
+    return Date.now();
+  }
+}
+
+interface IState {
+  currentTask: string;
+  tasks: Array<ITask>;
+}
+
+interface ITask {
+  id: number;
+  value: string;
+  completed: boolean;
+}
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/i,
+        // обработка идет справа налево
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+};
